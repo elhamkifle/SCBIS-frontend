ae9a5c648e97f5b01bd354fb4622ae4d1d719e27
fanual-software-dev <asfawfanual2003@gmail.com>
2025-05-25 15:37:54 +0300

    merge confilict  resolved

--- scbis-frontend/schema/zodSchema.ts
+++ scbis-frontend/schema/zodSchema.ts
@@ -1,10 +1,8 @@
-import { Phone } from "lucide-react";
 import { z } from "zod";
 
 export const AuthSchema = z.object({
-    email: z.string().email("Invalid email address"),
+    email: z.string().min(1, "Email or phone number is required"),
     password: z.string().min(8,"Password must be at least 8 characters long"),
-    confirmPassword: z.string().min(8,"Password must be at least 8 characters long"),
 })
 
 export const ForgotPasswordSchema = z.object({
