048a0265d11036394b595a19efec7bb38815e233
fanual-software-dev <asfawfanual2003@gmail.com>
2025-06-14 19:52:28 +0300

    Stashed changes to be committed

--- scbis-frontend/components/PolicyPurchase/CustomerInformation/uploadID.tsx
+++ scbis-frontend/components/PolicyPurchase/CustomerInformation/uploadID.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { useRouter } from 'next/navigation';
 import { useUploadIDStore } from '@/store/customerInformationStore/uploadID';
 import { usePersonalDetailStore } from '@/store/customerInformationStore/personalDetails';
-// import { useAddressStore } from '@/store/customerInformationStore/addressStore'; 
+import { useAddressStore } from '@/store/customerInformationStore/addressStore'; 
 
 export default function UploadIDForm() {
     const router = useRouter();
